931cc5d4bd5a70dc5c17428c35f18ede0fc0618b
nima moradi <mnima8100@gmail.com>
2024-08-04 12:09:08 +0330

    profile mockup

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,11 +1,21 @@
+'use client'
+
 import CenterWidthWrapper from '@/components/CenterWidthWrapper'
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
+    <>
     <CenterWidthWrapper>
-        <p>User Profile Page</p>
+        <div className='flex flex-col md:flex-row justify-center items-center gap-5 md:gap-10'>
+            <img alt='user' src='/user-1.png' className='rounded-3xl'/>
+        <p className='text-xl'>User Profile Page</p>
+        </div>
     </CenterWidthWrapper>
+    <Navbar />
+    </>
+    
   )
 }
 
