f14d064e2cfd7ad0cec9a0e7c806ed7a618017c0
nima moradi <mnima8100@gmail.com>
2024-08-15 01:19:37 +0330

    ui modification

--- app/transactions/Transactions.tsx
+++ app/transactions/Transactions.tsx
@@ -36,7 +36,7 @@ const Transactions = () => {
     if (error) 
       return(
         <MaxWidthWrapper>
-        <div className='flex flex-col gap-3 items-center justify-center w-full h-[30dvh]'>
+        <div className='flex flex-col gap-3 text-center items-center justify-center w-full h-[30dvh]'>
             <p className='font-bold text-lg'>There are nothing</p>
             <p className='text-sm text-neutral-500'>Your recent transactions are shown here</p>
             <p className='text-3xl font-light border-2 border-blue-600 rounded-full py-2 px-5 text-blue-600'>!</p>
@@ -51,16 +51,16 @@ if(data) {
           <div key={trx.id}  className='bg-red-50 rounded-2xl p-5 px-8 w-full'>
        <div className='flex items-center justify-between w-full'>
          <div className='flex flex-col items-start justify-start gap-1'>
-           <h2 className='font-bold text-base text-black'>
+           <h2 className='font-bold text-xs text-black'>
              {trx.description}
            </h2>
-           <h4 className='text-sm text-zinc-500'>
+           <h4 className='text-xs text-zinc-500'>
              {formatDate(trx.transactionDate)}
            </h4>
          </div>
          <div>
            <p
-           className={`${trx.amount >= 0 ? "text-green-600" : "text-red-600"} font-bold text-lg `}
+           className={`${trx.amount >= 0 ? "text-green-600" : "text-red-600"} font-bold text-base `}
            > {trx.amount >=0 ? <span>+</span> : null}
              ${trx.amount}</p>
          </div>
